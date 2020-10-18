9f7dbd2c7df3f34242ffd5c9cc0a3de7ef918511
Mikel Sagardia <mxagar@gmail.com>
2020-10-18 16:36:19 +0200

    compilation issues

--- opencv/c++/opencv_example_1/videoStreaming.cpp
+++ opencv/c++/opencv_example_1/videoStreaming.cpp
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv;
 int main(int argc, char** argv )
 {
-
     // Video input object: 0 firsrt cam, 1 next, etc.
     VideoCapture cap(0);
     if (!cap.isOpened())
@@ -19,5 +16,5 @@ int main(int argc, char** argv )
             break;
         }
     }
-
+    return 0;
 }
