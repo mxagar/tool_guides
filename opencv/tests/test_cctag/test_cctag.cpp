5f9b7fb310e0bea706ed4909a849b97a4adfb1e3
msagardia <msagardia@vicomtech.org>
2020-11-18 14:57:22 +0100

    Compilation of an empty example: header files included only, no real implementation inside

--- opencv/tests/test_cctag/test_cctag.cpp
+++ opencv/tests/test_cctag/test_cctag.cpp
@@ -2,12 +2,23 @@
 * 
 */
 
+#include "cctag/utils/FileDebug.hpp"
+#include "cctag/utils/VisualDebug.hpp"
+#include "cctag/utils/Exceptions.hpp"
+#include "cctag/Detection.hpp"
+#ifdef CCTAG_WITH_CUDA
+#include "cctag/cuda/device_prop.hpp"
+#include "cctag/cuda/debug_macros.hpp"
+#endif // CCTAG_WITH_CUDA
+
 #include <iostream>
 #include <string>
 
+using namespace cctag;
 
 int main(int argc, char *argv[]) {
 
+    std::cout << "Test" << std::endl;
     return 0;
 
 }
