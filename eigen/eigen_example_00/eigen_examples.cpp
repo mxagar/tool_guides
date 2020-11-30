c46b8e6bc9c6b6c9803b9fecf032cc42163008c4
Mikel Sagardia <mxagar@gmail.com>
2020-11-30 08:51:01 +0100

    comments

--- eigen/eigen_example_00/eigen_examples.cpp
+++ eigen/eigen_example_00/eigen_examples.cpp
@@ -5,10 +5,15 @@ using Eigen::MatrixXd;
 
 int main()
 {
+    // Matrix
+
     MatrixXd m(2, 2);
     m(0, 0) = 3;
     m(1, 0) = 2.5;
     m(0, 1) = -1;
     m(1, 1) = m(1, 0) + m(0, 1);
     std::cout << m << std::endl;
+
+    
+
 }
