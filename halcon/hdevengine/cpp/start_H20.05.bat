923cfd992a1d8a70fff223574d33bd4f1bf52d1e
msagardia <msagardia@vicomtech.org>
2021-02-17 16:52:25 +0100

    removed files

--- halcon/hdevengine/cpp/start_H20.05.bat
+++
@@ -1,9 +0,0 @@
-:: Set echo off
-@ECHO OFF
-:: Set Halcon environment variables for this cmd propt
-set HALCONROOT="C:\Program Files\MVTec\HALCON-20.05-Progress"
-set HALCONARCH=x64-win64
-set HALCONEXAMPLES=C:\Users\Public\Documents\MVTec\HALCON-20.05-Progress\examples
-set HALCONIMAGES=%HALCONEXAMPLES%\images
-:: Start HDevelop
-:: "C:\Program Files\MVTec\HALCON-20.05-Progress\bin\x64-win64\hdevelop.exe"
